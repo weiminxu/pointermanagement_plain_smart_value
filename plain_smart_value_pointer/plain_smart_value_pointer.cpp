87b72351b6046884d4b4dc0e65f039d62a5546c9
weiminxu <weiminxu1982@gmail.com>
2019-12-16 23:06:29 +1300

    plain pointer
    plain pointer

---
+++ plain_smart_value_pointer/plain_smart_value_pointer.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "plain_pointer.h"
+
+using namespace std;
+
+void test_plainpointer()
+{
+	int i = 42;
+	AHasPtr p1(&i, 42);
+	AHasPtr p2 = p1;
+	cout << p2.get_ptr_val() << endl;
+
+	p1.set_ptr_val(0);
+	cout << p2.get_ptr_val() << endl;
+
+}
+
+int main()
+{
+	test_plainpointer();
+
+	return 0;
+}
