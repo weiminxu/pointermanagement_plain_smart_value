64b2e8060f45ee14d314740e2cb97ad81c03e3bf
weiminxu <weiminxu1982@gmail.com>
2019-12-17 00:45:34 +1300

    different way for parameters list
    different way for parameters list

--- plain_smart_value_pointer/plain_pointer.h
+++ plain_smart_value_pointer/plain_pointer.h
@@ -7,6 +7,7 @@ private:
 	int* ptr;
 public:
 	AHasPtr(int *p, int i):ptr(p),val(i) {}
+	//AHasPtr(const int &p, int i):ptr(new int(p)),val(i){}
 
 	int* get_ptr() const { return ptr;}
 	int get_int() const { return val;}
@@ -16,7 +17,5 @@ public:
 
 	int get_ptr_val() const { return *ptr; }
 	void set_ptr_val(int val) const { *ptr = val; }
-
-
 
 };
