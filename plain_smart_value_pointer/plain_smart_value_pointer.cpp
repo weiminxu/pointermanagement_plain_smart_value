22acae23c140f50f1dd36a0d74c8ead85a981318
weiminxu <weiminxu1982@gmail.com>
2019-12-18 18:42:45 +1300

    value pointer
    value pointer

--- plain_smart_value_pointer/plain_smart_value_pointer.cpp
+++ plain_smart_value_pointer/plain_smart_value_pointer.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "plain_pointer.h"
+#include "value_ptr.h"
 
 using namespace std;
 
@@ -13,16 +14,29 @@ void test_plainpointer()
 	p1.set_ptr_val(0);
 	cout << p2.get_ptr_val() << endl;
 
+	//it is easy to generate wild pointer
+	/*
 	int* ip = new int(42);
 	AHasPtr ptr(ip, 10);
 	cout << ptr.get_ptr_val() << endl;
 	delete ip;
 	cout << ptr.get_ptr_val() << endl;
+	*/
+}
+
+void test_valuepointer()
+{
+	int obj = 0;
+
+	CHasPtr ptr1(obj, 42);
+	CHasPtr ptr2(ptr1);
+	cout << ptr1.get_ptr_val() << ", " << ptr1.get_int() << endl;
 }
 
 int main()
 {
-	test_plainpointer();
+	//test_plainpointer();
+	test_valuepointer();
 
 	return 0;
 }
--- plain_smart_value_pointer/value_ptr.h
+++ plain_smart_value_pointer/value_ptr.h
@@ -1,1 +1,31 @@
 #pragma once
+
+class CHasPtr
+{
+private:
+	int val;
+	int* ptr;
+public:
+	//CHasPtr(int* p, int i) :ptr(p), val(i) {}
+	CHasPtr(const int& p, int i) :ptr(new int(p)), val(i) {}
+	CHasPtr(const CHasPtr& orig) :ptr(new int(*orig.ptr)), val(orig.val) {}//copy constructure
+	CHasPtr& operator=(const CHasPtr&);//copy operator
+	~CHasPtr() { delete ptr; }
+
+	int* get_ptr() const { return ptr; }
+	int get_int() const { return val; }
+
+	void set_ptr(int* p) { ptr = p; }
+	void set_int(int i) { val = i; }
+
+	int get_ptr_val() const { return *ptr; }
+	void set_ptr_val(int val) const { *ptr = val; }
+
+};
+
+CHasPtr& CHasPtr::operator=(const CHasPtr& rhs)
+{
+	*ptr = *rhs.ptr;
+	val = rhs.val;
+	return *this;
+}
